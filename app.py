6ff1d7261a912b4c3381b2545123f863b0e09e6e
jfewz2 <james@dataplumbers.co.uk>
2024-08-10 14:27:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,10 @@
-from flask import Flask, request, send_file, render_template
-from pytube import YouTube
-import os
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('Template.html')
-
-@app.route('/download', methods=['POST'])
-def download_video():
-    youtube_url = request.form['youtube_url']
-    try:
-        yt = YouTube(youtube_url)
-        stream = yt.streams.filter(progressive=True, file_extension='mp4').first()
-        download_path = stream.download()
-        return send_file(download_path, as_attachment=True)
-    except Exception as e:
-        return str(e), 500
+def hello_world():
+    return 'Hello, Azure!'
 
 if __name__ == '__main__':
     app.run(debug=True)
